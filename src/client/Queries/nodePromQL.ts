2fbc29c16efed869552986ee96a73a9df0d68985
Murad Alqadi <95775749+murad-alqadi@users.noreply.github.com>
2022-08-24 11:39:57 -0500

    Refactoring frontend promql query handling
    - Offload secure information from frontend to backend as security feature
    
    -  @rich9029 will complete refactoring

--- src/client/Queries/nodePromQL.ts
+++ src/client/Queries/nodePromQL.ts
@@ -1,36 +1,37 @@
-/*
-look into what regenerator runtime this and if it's necessary
-- says it is something to do with babel and async
- //import 'regenerator-runtime/runtime'; 
+import { Get } from '../Services';
+import { apiRoute } from '../utils';
 
-*/
 
- //return node CPU usage as a number
- export const fetchCpuUsage = async() => {
-   const data = await fetch('http://localhost:30000/api/v1/query?query=(1 - sum by (instance)(increase(node_cpu_seconds_total{mode="idle"}[5m])) / sum by (instance)(increase(node_cpu_seconds_total[5m])))*100', {
-     method: 'GET',
-     headers: {
-       'Accept': 'application/json',
-       'Content-Type': 'application/json'
-     }
-   })
-   .then(res => res.json())
-   const cpuUsage = parseInt(data.data.result[0].value[1]);
-   return cpuUsage;
- }
- 
- //return node memory usage as a number
- export const fetchMemoryUsage = async(nodeName) => {
-   const data = await fetch(`http://localhost:30000/api/v1/query?query=(1-sum(kube_node_status_allocatable{resource="memory",unit="byte",node="${nodeName}"})/sum(kube_node_status_capacity{resource="memory",unit="byte",node="${nodeName}"}))*100`, {
-     method: 'GET',
-     headers: {
-       'Accept': 'application/json',
-       'Content-Type': 'application/json'
-     }
-   }).then(res => res.json())
-   const memoryUsage = data.data.result[0].value[1];
-   return memoryUsage;
- }
+export const nodeMetric = {
+  cpuLoad: async (clusterId: number, ns: string) => {
+    const query = '(1 - sum by (instance)(increase(node_cpu_seconds_total{mode="idle"}[5m])) / sum by (instance)(increase(node_cpu_seconds_total[5m])))*100'
+    try {
+      const metric = await Get(apiRoute.getRoute(
+        `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+      ))
+        .then(res => res.json());
+      const cpuLoad= parseInt(metric.data.result[0].value[1]);
+      return cpuLoad;  
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  memoryLoad: async(clusterId: number, ns: string, node: string) => {
+    const query = `(1-sum(kube_node_status_allocatable{resource="memory",unit="byte",node="${node}"})/sum(kube_node_status_capacity{resource="memory",unit="byte",node="${node}"}))*100`
+    try {
+      const metric = await Get(apiRoute.getRoute(
+        `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+      ))
+        .then(res => res.json());
+      const memoryLoad = metric.data.result[0].value[1];
+      return memoryLoad;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+}
+
+
  
  // return all pods from a node
  export const fetchNodePods= async(nodeName) => {
