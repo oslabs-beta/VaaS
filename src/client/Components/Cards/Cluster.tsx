96272f84d02e3a6e52c29cc86917536e87c08b8f
Kevin Le <kevinle1003@gmail.com>
2022-08-24 21:24:40 -0500

    cluster metrics wip

--- src/client/Components/Cards/Cluster.tsx
+++ src/client/Components/Cards/Cluster.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { clusterMetric } from '../../Queries/clusterPromQL';
+import { nodeMetric } from '../../Queries/nodePromQL';
 import { ClusterTypes } from '../../Interfaces/ICluster';
 
 import './styles.css';
@@ -8,7 +9,7 @@ const Cluster = (props: ClusterTypes) => {
   const [clusterName, setClusterName] = useState<string | undefined>('');
   const [description, setDescription] = useState<string | undefined>('');
   const [nodeName, setNodeName] = useState('');
-  const [cpuUsage, setCpuUsage] = useState('');
+  const [cpuUsage, setCpuUsage] = useState<number | undefined>(0);
   const [memoryUsage, setMemoryUsage] = useState('');
   const [totalDeployments, setTotalDeployments] = useState('');
   const [totalPods, setTotalPods] = useState('');
@@ -22,15 +23,28 @@ const Cluster = (props: ClusterTypes) => {
     fetchNodes();
     setClusterName(props.name);
     setDescription(props.description);
-    // const fetchCpuUsage = async () => {
-      
-    // }
-    setCpuUsage('');
-    //nodeMetric.cpuLoad
-    setMemoryUsage('');
-    //nodeMetric.memoryLoad
+    const fetchCpuUsage = async () => {
+      const cpuUsage = await nodeMetric.cpuLoad(props._id, 'k8');
+      setCpuUsage(cpuUsage);
+    };
+    fetchCpuUsage();
+    const fetchMemoryUsage = async () => {
+      const memoryUsage = await nodeMetric.memoryLoad(props._id, 'k8', nodeName);
+      setMemoryUsage(memoryUsage);
+    };
+    fetchMemoryUsage();
+
+    const fetchTotalDeployments = async () => {
+      const totalDeployments = await clusterMetric.totalDeployments(props._id, 'k8');
+      setTotalDeployments(totalDeployments.length);
+    };
+    fetchTotalDeployments();
     setTotalDeployments('');
-    setTotalPods('');
+    const fetchTotalPod = async () => {
+      const totalPods = await clusterMetric.totalPods(props._id, 'k8');
+      setTotalPods(totalPods.length);
+    };
+    fetchTotalPod();
   }, []);
 
   return (
@@ -38,7 +52,7 @@ const Cluster = (props: ClusterTypes) => {
       <p>{'Cluster name: ' + clusterName}</p>
       <p>{'Description: ' + description}</p>
       <p>{'Node: ' + nodeName}</p>
-      <p>{'CPU usage: ' + cpuUsage}</p>
+      <p>{'CPU usage: ' + cpuUsage + '%'}</p>
       <p>{'Memory usage: ' + memoryUsage}</p>
       <p>{'Total deployments: '  + totalDeployments}</p>
       <p>{'Total Pods: ' + totalPods}</p>
--- src/client/Queries/clusterPromQL.ts
+++ src/client/Queries/clusterPromQL.ts
@@ -27,24 +27,24 @@ export const clusterMetric = {
       console.log(err);
     }
   },
-  totalDeployments: async(clusterId: string, ns: string) => {
+  totalDeployments: async(clusterId: string | undefined, ns: string) => {
     const query = 'kube_deployment_created';
     try {
       const metric = await Get(apiRoute.getRoute(
         `/prom?id=${clusterId}&ns=${ns}&q=${query}`
-      ));
+      ), clusterMetric.token);
       const totalDeployments = metric.data.result;
       return totalDeployments;  
     } catch (err) {
       console.log(err);
     }
   },
-  totalPods: async(clusterId: string, ns: string) => {
+  totalPods: async(clusterId: string | undefined, ns: string) => {
     const query = 'count(kube_pod_created)';
     try {
       const metric = await Get(apiRoute.getRoute(
         `/prom?id=${clusterId}&ns=${ns}&q=${query}`
-      ));
+      ), clusterMetric.token);
       const totalPods = metric.data.result[0].value[1];
       return totalPods;  
     } catch (err) {
@@ -56,7 +56,7 @@ export const clusterMetric = {
     try {
       const metric = await Get(apiRoute.getRoute(
         `/prom?id=${clusterId}&ns=${ns}&q=${query}`
-      ));
+      ), clusterMetric.token);
       const allServices = metric.data.result;
       return allServices;  
     } catch (err) {
@@ -68,7 +68,7 @@ export const clusterMetric = {
     try {
       const metric = await Get(apiRoute.getRoute(
         `/prom?id=${clusterId}&ns=${ns}&q=${query}`
-      ));
+      ), clusterMetric.token);
       const namespaces = metric.data.result;
       return namespaces;
     } catch (err) {
