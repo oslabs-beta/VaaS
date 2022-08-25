8da3ac19724ddc20bc061a0f66c2afe63f3a11c1
irvin <irvinwie@gmail.com>
2022-08-24 18:30:10 -0700

    added podPromQL and containerPromQL

--- src/client/Queries/podPromQL.ts
+++ src/client/Queries/podPromQL.ts
@@ -1,34 +1,23 @@
 import { Get } from '../Services';
 import { apiRoute } from '../utils';
 
-// return a pod names as an array of strings
-export const fetchPodNamesList = async (nodeName) => {
-    const data = await fetch(`http://localhost:30000/api/v1/query?query=kube_pod_info{node="${nodeName}"}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json());
-    const podNamesList = data.data.result.map(result => {
+export const podMetric = {
+  namesList: async (clusterId: string, ns: string, node: string) => {
+    const query = `kube_pod_info{node="${node}"}`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return metric.data.result.map(result => {
       return result.metric.pod;
     });
-    return podNamesList;  
-  };
-  
-  // return a pod info as an array of objects with {name, node, namespace, ip, deployment}
-  export const fetchPodInfoList = async (nodeName) => {
-    const data = await fetch(`http://localhost:30000/api/v1/query?query=kube_pod_info{node="${nodeName}"}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json());
-    const podInfoList = data.data.result.map(result => {
-      return { 
+  },
+  infoList: async (clusterId: string, ns: string, node: string) => {
+    const query = `kube_pod_info{node="${node}"}`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return metric.data.result.map(result => {
+      return {
         podName: result.metric.pod, 
         podNamespace: result.metric.namespace, 
         podIp: result.metric.pod_ip, 
@@ -36,24 +25,19 @@ export const fetchPodNamesList = async (nodeName) => {
         uid: result.metric.uid
       };
     });
-    return podInfoList;  
-  };
-  
-  export const fetchCurrentPodInfo = async (currentPod) => {
-    const data = await fetch(`http://localhost:30000/api/v1/query?query=kube_pod_info{pod="${currentPod}"}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json());
-    const currentPodInfo = {
-      podName: data.data.result[0].metric.pod, 
-      podNamespace: data.data.result[0].metric.namespace, 
-      podIp: data.data.result[0].metric.pod_ip, 
-      createdByDeployment: data.data.result[0].metric.created_by_name,
-      uid: data.data.result[0].metric.uid
+  },
+  podInfo: async (clusterId: string, ns: string, pod: string) => {
+    const query = `kube_pod_info{pod="${pod}"}`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return {
+      podName: metric.data.result[0].metric.pod, 
+      podNamespace: metric.data.result[0].metric.namespace, 
+      podIp: metric.data.result[0].metric.pod_ip, 
+      createdByDeployment: metric.data.result[0].metric.created_by_name,
+      uid: metric.data.result[0].metric.uid
     };
-    return currentPodInfo;  
-  };
+  }
+};
+
