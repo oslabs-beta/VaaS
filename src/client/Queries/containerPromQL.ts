33549728800959cfe09135bf840d718dd39f8948
irvin <irvinwie@gmail.com>
2022-08-24 18:55:18 -0700

    Finished queries for containerPromQL and podPromQL

--- src/client/Queries/containerPromQL.ts
+++ src/client/Queries/containerPromQL.ts
@@ -8,10 +8,10 @@ export const containerMetric = {
       `/prom?id=${clusterId}&ns=${ns}&q=${query}`
     ));
     return metric.data.result.map(result => {
-      return [ result.metric.container, result.metric.pod]
+      return [ result.metric.container, result.metric.pod];
     });
   },
-  cpuUsage: async (clusterId: string, ns: string, container: string, startTime:, endTime:) => {
+  cpuUsage: async (clusterId: string, ns: string, container: string) => {
     const query = `rate(container_cpu_usage_seconds_total{container="${container}"}[10m])`;
     const metric = await Get(apiRoute.getRoute(
       `/prom?id=${clusterId}&ns=${ns}&q=${query}`
@@ -19,62 +19,31 @@ export const containerMetric = {
     return metric.data.result[0].value;
   },
   rangeContainerCpuUsage: async (clusterId: string, ns: string, container: string, startTime: number, endTime: number) => {
-    const query = `rate(container_cpu_usage_seconds_total{container="${container}"}[10m])&start=${startTime}&end=${endTime}&step=60s`
+    const query = `rate(container_cpu_usage_seconds_total{container="${container}"}[10m])&start=${startTime}&end=${endTime}&step=60s`;
     const metric = await Get(apiRoute.getRoute(
       `/prom?id=${clusterId}&ns=${ns}&q=${query}`
     ));
-    return metric.data.result[0].values
+    return metric.data.result[0].values;
   },
-  rangeContainerCpuSaturation: async (clusterId: string, ns: string, container: string, startTime:number, endTime:number) => {
+  rangeContainerCpuSaturation: async (clusterId: string, ns: string, container: string, startTime: number, endTime: number) => {
     const query = `rate(container_cpu_cfs_throttled_seconds_total{container="${container}"}[10m])&start=${startTime}&end=${endTime}&step=60s`;
     const metric = await Get(apiRoute.getRoute(
       `/prom?id=${clusterId}&ns=${ns}&q=${query}`
     ));
     return metric.data.result.length === 0 ? 0 : metric.data.result[0].values;
   },
-
-};
-
-
-
-
-// returns an array of Memory usage values between an input start and end time, in 60s intervals
-// for each element in the array, element[0] = unixtimestamp, element[1] = Memory working in bytes
-// working_set_bytes: more realistic measurement than usage_bytes
-export const fetchRangeContainerMemoryUsage = async (containerName, startTime, endTime) => {
-  const data = await fetch(`http://localhost:30000/api/v1/query_range?query=container_memory_working_set_bytes{container="${containerName}"}&start=${startTime}&end=${endTime}&step=60s`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(res => res.json());
-
-  const rangeContainerMemoryUsage = data.data.result[0].values;
-  return rangeContainerMemoryUsage;  
-};
-
-// returns an array of Memory saturation values between an input start and end time, in 60s intervals
-// for each element in the array, element[0] = unixtimestamp, element[1] = Memory saturation as a ratio of memory in use against memory limit allocated to container
-// NOTE: kube_pod_container_resource_limits is experimental, meaning it could change at any time. 
-// possibly can be replaced by container_spec_memory_limit_bytes, which returns 0 if no limit is set
-// info on setting memory limits: https://sysdig.com/blog/kubernetes-resource-limits/
-export const fetchRangeContainerMemorySaturation = async (containerName, startTime, endTime) => {
-  let rangeContainerMemorySaturation;
-  const data = await fetch(`http://localhost:30000/api/v1/query_range?query=sum(container_memory_working_set_bytes{container="${containerName}"})%20/%20sum(kube_pod_container_resource_limits{resource="memory",unit="byte",container="${containerName}"})&start=${startTime}&end=${endTime}&step=60s`, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(res => res.json());
-  if (data.data.result = []) {
-    rangeContainerMemorySaturation = 0;
-  } else {
-    rangeContainerMemorySaturation = data.data.result[0].values;
+  rangeContainerMemoryUsage: async (clusterId: string, ns: string, container: string, startTime: number, endTime: number) => {
+    const query = `container_memory_working_set_bytes{container="${container}"}&start=${startTime}&end=${endTime}&step=60s`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return metric.data.result[0].values;
+  },
+  rangeContainerMemorySaturation: async (clusterId: string, ns: string, container: string, startTime: number, endTime: number) => {
+    const query = `sum(container_memory_working_set_bytes{container="${container}"})%20/%20sum(kube_pod_container_resource_limits{resource="memory",unit="byte",container="${container}"})&start=${startTime}&end=${endTime}&step=60s`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return metric.data.result.length === 0 ? 0 : metric.data.result[0].values;
   }
-
-  return rangeContainerMemorySaturation;  
 };
--- src/client/Queries/podPromQL.ts
+++ src/client/Queries/podPromQL.ts
@@ -10,21 +10,14 @@ export const podMetric = {
     return metric.data.result.map(result => {
       return result.metric.pod;
     });
-    return podNamesList;  
-  };
-  
-  // return a pod info as an array of objects with {name, node, namespace, ip, deployment}
-  export const fetchPodInfoList = async (nodeName) => {
-    const data = await fetch(`http://localhost:30000/api/v1/query?query=kube_pod_info{node="${nodeName}"}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json());
-    const podInfoList = data.data.result.map(result => {
-      return { 
+  },
+  infoList: async (clusterId: string, ns: string, node: string) => {
+    const query = `kube_pod_info{node="${node}"}`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return metric.data.result.map(result => {
+      return {
         podName: result.metric.pod, 
         podNamespace: result.metric.namespace, 
         podIp: result.metric.pod_ip, 
@@ -32,24 +25,18 @@ export const podMetric = {
         uid: result.metric.uid
       };
     });
-    return podInfoList;  
-  };
-  
-  export const fetchCurrentPodInfo = async (currentPod) => {
-    const data = await fetch(`http://localhost:30000/api/v1/query?query=kube_pod_info{pod="${currentPod}"}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(res => res.json());
-    const currentPodInfo = {
-      podName: data.data.result[0].metric.pod, 
-      podNamespace: data.data.result[0].metric.namespace, 
-      podIp: data.data.result[0].metric.pod_ip, 
-      createdByDeployment: data.data.result[0].metric.created_by_name,
-      uid: data.data.result[0].metric.uid
+  },
+  podInfo: async (clusterId: string, ns: string, pod: string) => {
+    const query = `kube_pod_info{pod="${pod}"}`;
+    const metric = await Get(apiRoute.getRoute(
+      `/prom?id=${clusterId}&ns=${ns}&q=${query}`
+    ));
+    return {
+      podName: metric.data.result[0].metric.pod, 
+      podNamespace: metric.data.result[0].metric.namespace, 
+      podIp: metric.data.result[0].metric.pod_ip, 
+      createdByDeployment: metric.data.result[0].metric.created_by_name,
+      uid: metric.data.result[0].metric.uid
     };
-    return currentPodInfo;  
-  };
+  }
+};
