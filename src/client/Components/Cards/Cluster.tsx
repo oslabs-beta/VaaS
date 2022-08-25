bc4f77d27b8c503230123d08b0fc2e94855a68ce
irvin <irvinwie@gmail.com>
2022-08-25 13:43:48 -0700

    added cluster styling

--- src/client/Components/Cards/Cluster.tsx
+++ src/client/Components/Cards/Cluster.tsx
@@ -7,6 +7,14 @@ import { Put } from '../../Services';
 import { apiRoute } from '../../utils';
 import Visualizer from '../Visualizer/Visualizer';
 import ClusterSettings from '../ClusterSettings/ClusterSettings';
+import { Container, Box } from '@mui/system';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 const Cluster = (props: ClusterTypes) => {
@@ -87,36 +95,50 @@ const Cluster = (props: ClusterTypes) => {
   };
 
   return (
-    <div id='cluster-card'>
+    <Container sx={{
+      height: '50vh',
+      minWidth: '100%',
+      justifyContent: 'center',
+      display: 'flex',
+      direction: 'column',
+      textAlign: 'left',
+      backgroundSize: 'contain',
+      bgcolor: '#3a4a5b'
+      
+    }} id="cluster-card">
+
       <div>
         <button id="favorite-btn" onClick={handleFavorite}>Favorite</button>
         <button id="favorite-btn" onClick={handleVisualizer}>Visualizer</button>
         <button id="favorite-btn" onClick={handleSettings}>Settings</button>
       </div>
+
       <div>
         <h2>{'' + clusterName}</h2>
         <p>{'' + description}</p>
         <p>{'Favorite Status:' + props.favoriteStatus}</p>
       </div>
-      <table>
-        <tr>
-          <td>Node</td>
-          <td>CPU Usage</td>
-          <td>Memory Usage</td>
-          <td>Total Deployments</td>
-          <td>Total Pods</td>
-        </tr>
-        <tr>
-          <td>{'' + nodeName}</td>
-          <td>{'' + cpuUsage + '%'}</td>
-          <td>{'' + memoryUsage}</td>
-          <td>{'' + totalDeployments}</td>
-          <td>{'' + totalPods}</td>
-        </tr>
-      </table>
+      <TableContainer component={Paper}>
+        <Table aria-label="spanning table">
+          <TableRow>
+            <TableCell align="center">Node</TableCell>
+            <TableCell align="center">CPU Usage</TableCell>
+            <TableCell align="center">Memory Usage</TableCell>
+            <TableCell align="center">Total Deployments</TableCell>
+            <TableCell align="center">Total Pods</TableCell>
+          </TableRow>
+        <TableBody>
+          <TableCell align="center">{'' + nodeName}</TableCell>
+          <TableCell align="center">{'' + cpuUsage + '%'}</TableCell>
+          <TableCell align="center">{'' + memoryUsage}</TableCell>
+          <TableCell align="center">{'' + totalDeployments}</TableCell>
+          <TableCell align="center">{'' + totalPods}</TableCell>
+        </TableBody>
+        </Table>
+      </TableContainer>
       {visualizer && <Visualizer />}
       {settings && <ClusterSettings />}
-    </div>
+    </Container>
   );
 };
 
