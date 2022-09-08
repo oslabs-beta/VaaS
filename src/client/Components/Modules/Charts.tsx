d946fbe121efcd1066488eed898595e2dee2a734
Murad Alqadi <95775749+murad-alqadi@users.noreply.github.com>
2022-09-08 15:36:13 -0500

    Final UI Cleanup
    - Modules overhauled

--- src/client/Components/Modules/Charts.tsx
+++ src/client/Components/Modules/Charts.tsx
@@ -3,7 +3,6 @@ import { Modules } from "../../Interfaces/ICluster";
 import { Delete, Get, Post } from "../../Services";
 import { apiRoute } from "../../utils";
 import { useLocation } from "react-router-dom";
-import "./styles.css";
 
 const Charts = (props: Modules) => {
   const { state }: any = useLocation();
