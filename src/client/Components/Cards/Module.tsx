2011ce5542b85d9aa3f23b8f6e2c4449a41494d9
James Chan <108029209+j-chany@users.noreply.github.com>
2022-10-08 10:37:57 -0700

    added function cost calculator module

--- src/client/Components/Cards/Module.tsx
+++ src/client/Components/Cards/Module.tsx
@@ -4,6 +4,7 @@ import OpenFaaS from "../Modules/OpenFaaS";
 import Visualizer from "../Modules/Visualizer";
 import CustomQuery from "../Modules/CustomQuery";
 import Charts from "../Modules/Charts";
+import FunctionCost from "../Modules/FunctionCost";
 import NavBar from "../Home/NavBar";
 import { Modules } from "../../Interfaces/ICluster";
 import Container from "@mui/system/Container";
@@ -15,6 +16,8 @@ import GrainIcon from "@mui/icons-material/Grain";
 import DataObjectIcon from '@mui/icons-material/DataObject';
 import FunctionsIcon from '@mui/icons-material/Functions';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+
 import "./styles.css";
 
 // needs to be chnaged to redux, under UI reducer ?
@@ -23,6 +26,7 @@ const Module = (props: Modules) => {
   const navigate = useNavigate();
   const [faas, setFaaS] = useState(true);
   const [visualizer, setVisualizer] = useState(false);
+  const [functionCost, setFunctionCost] = useState(false);
   const [custom, setCustom] = useState(false);
   const [charts, setCharts] = useState(false);
   const [currentModule, setCurrentModule] = useState("module");
@@ -62,17 +66,20 @@ const Module = (props: Modules) => {
             setFaaS(true);
             setVisualizer(false);
             setCustom(false);
+            setFunctionCost(false);
             setCharts(false);
             break;
           case "visualizer":
             setFaaS(false);
             setVisualizer(true);
             setCustom(false);
+            setFunctionCost(false);
             setCharts(false);
             break;
           case "custom":
             setFaaS(false);
             setVisualizer(false);
+            setFunctionCost(false);
             setCustom(true);
             setCharts(false);
             break;
@@ -80,7 +87,15 @@ const Module = (props: Modules) => {
             setFaaS(false);
             setVisualizer(false);
             setCustom(false);
+            setFunctionCost(false);
             setCharts(true);
+            break;
+          case "functionCost":
+            setFaaS(false);
+            setVisualizer(false);
+            setCustom(false);
+            setFunctionCost(true);
+            setCharts(false);
             break;
         }
       }
@@ -91,6 +106,7 @@ const Module = (props: Modules) => {
     setFaaS(true);
     setCurrentModule("faas");
     setVisualizer(false);
+    setFunctionCost(false);
     setCustom(false);
     setCharts(false);
   };
@@ -100,6 +116,7 @@ const Module = (props: Modules) => {
     setVisualizer(true);
     setCurrentModule("visualizer");
     setCustom(false);
+    setFunctionCost(false);
     setCharts(false);
   };
 
@@ -108,6 +125,7 @@ const Module = (props: Modules) => {
     setVisualizer(false);
     setCustom(true);
     setCurrentModule("custom");
+    setFunctionCost(false);
     setCharts(false);
   };
 
@@ -116,9 +134,19 @@ const Module = (props: Modules) => {
     setVisualizer(false);
     setCustom(false);
     setCharts(true);
+    setFunctionCost(false);
     setCurrentModule("charts");
   };
 
+  const handleFunctionCostButton = () => {
+    console.log('CLICKED');
+    setFaaS(false);
+    setVisualizer(false);
+    setCustom(false);
+    setCharts(false);
+    setFunctionCost(true);
+    setCurrentModule("functionCost");
+  };
   return (
     <div>
       <Container 
@@ -152,6 +180,12 @@ const Module = (props: Modules) => {
                 Charts
               </div>
             }
+            {
+              functionCost &&
+              <div>
+                OpenFaaS Function Cost Calculator
+              </div>
+            }
           </div>
           <Button
             sx={buttonStyle}
@@ -178,7 +212,18 @@ const Module = (props: Modules) => {
             className="module-button"
             onClick={handleFaaSButton}
           >
-            <FunctionsIcon />
+          <FunctionsIcon />
+
+          </Button>
+          <Button
+            sx={buttonStyle}
+            variant="text"
+            id="basic-button"
+            className="module-button"
+            onClick={handleFunctionCostButton}
+          >
+          <AttachMoneyIcon />
+
           </Button>
           <Button
             sx={buttonStyle}
@@ -258,12 +303,20 @@ const Module = (props: Modules) => {
             />
           }
           {
+            functionCost &&
+            <FunctionCost
+            id={id}
+            nested={props.nested}
+            />
+          }
+          {
             visualizer && 
             <Visualizer               
               id={id} 
               nested={props.nested} 
             />
           }
+    
         </div>
       </Container>
       <Container 
--- src/client/Components/Modules/FunctionCost.tsx
+++ src/client/Components/Modules/FunctionCost.tsx
@@ -3,13 +3,32 @@ import { Modules } from "../../Interfaces/ICluster";
 import { Get } from "../../Services";
 import { apiRoute } from "../../utils";
 import { useAppDispatch, useAppSelector } from "../../Store/hooks";
+import { useLocation } from "react-router-dom";
 import { IReducers } from "../../Interfaces/IReducers";
+import { Container, TextField, Button } from '@mui/material';
 
 
 const FunctionCost = (props: Modules) => {
   const { state }: any = useLocation();
   const [id] = useState(props.id || state[0]);
+  const OFReducer = useAppSelector((state: IReducers) => state.OFReducer);
+  const deployedFunctions = OFReducer.deployedFunctions || [];
+
+  useEffect(() => {
+    // fullscreen mode adjustment  
+  }, []); 
 
 
-  // 
+  return (
+    <Container
+      sx={{
+        width: '100%',
+        textAlign: 'center'
+      }}
+    >
+    <div>HI</div>
+    </Container>
+  );
+
 }; 
+export default FunctionCost;
