a479b6bd25beb3c20e8f10bb09d567c5f249ece3
James Chan <108029209+j-chany@users.noreply.github.com>
2022-10-10 15:08:35 -0700

    added 2 charts in chart component

--- src/client/Components/Modules/Charts.tsx
+++ src/client/Components/Modules/Charts.tsx
@@ -15,6 +15,8 @@ const Charts = (props: Modules) => {
   return (
     <div>
       <iframe src="http://localhost:3001/d-solo/oWe9aYxmk/1-kubernetes-deployment-statefulset-daemonset-metrics?orgId=1&refresh=30s&from=1665408311835&to=1665419111835&panelId=2" width="450" height="200" frameBorder="0"></iframe>
+      <iframe src="http://localhost:3001/d-solo/oWe9aYxmk/1-kubernetes-deployment-statefulset-daemonset-metrics?orgId=1&refresh=30s&from=1665428866939&to=1665439666939&panelId=3" width="450" height="200" frameBorder="0"></iframe>
+      <iframe src="http://localhost:3001/d-solo/oWe9aYxmk/1-kubernetes-deployment-statefulset-daemonset-metrics?orgId=1&refresh=30s&from=1665428398962&to=1665439198962&panelId=11" width="450" height="200" frameBorder="0"></iframe>
     </div>
   );
 };
