c77be741b2e4b5584cb6a245bd460d7149c3cca4
Jimmy Lim <jimmylim@Jimmys-MBP.lan>
2022-10-10 09:25:46 -0700

    added grafana embedding

--- src/client/Components/Modules/Charts.tsx
+++ src/client/Components/Modules/Charts.tsx
@@ -7,14 +7,14 @@ import { useLocation } from "react-router-dom";
 const Charts = (props: Modules) => {
   const { state }: any = useLocation();
   const [id] = useState(props.id || state[0]);
-  
+
   useEffect(() => {
     console.log('placeholder');
   }, []);
 
   return (
     <div>
-
+      <iframe src="http://localhost:3001/d-solo/oWe9aYxmk/1-kubernetes-deployment-statefulset-daemonset-metrics?orgId=1&refresh=30s&from=1665408311835&to=1665419111835&panelId=2" width="450" height="200" frameBorder="0"></iframe>
     </div>
   );
 };
