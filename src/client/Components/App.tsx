37e735a5dfa7104ba5054d8062c20dfa3132ed55
ruqayaah <49168086+ruqayaahh@users.noreply.github.com>
2022-12-15 04:13:45 +0100

    remove commented out code

--- src/client/Components/App.tsx
+++ src/client/Components/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Login from './Login/Login';
 import Home from './Home/Home';
 import Register from './Login/Register';
@@ -23,7 +22,6 @@ const App = () => {
           <Route path="/module" element={<Module />} />
         </Route>
       </Routes>
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 };
