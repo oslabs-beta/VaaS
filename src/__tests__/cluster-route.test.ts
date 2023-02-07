12dc42d7eb238e69e17d0848cce4b74d5bb966bd
Terrence Granger <terrence.granger@gmail.com>
2023-02-07 17:26:26 -0500

    added windows test script

--- src/__tests__/cluster-route.test.ts
+++ src/__tests__/cluster-route.test.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { describe, it, expect, beforeAll, afterAll, afterEach } from 'vitest';
 import { AddClusterType } from '../client/Interfaces';
 import request from 'supertest';
 import app from './test-api';
 import { User, Cluster } from 'src/server/models';
 
-const testClusterOne: AddClusterType = {
+const testCluster1: AddClusterType = {
   url: 'cluster.test',
   k8_port: '9090',
   faas_port: '9090',
@@ -19,7 +19,7 @@ const testClusterOne: AddClusterType = {
   cost_port: '9090',
 };
 
-const testClusterTwo: AddClusterType = {
+const testCluster2: AddClusterType = {
   url: 'cluster.test',
   k8_port: '9090',
   faas_port: '9090',
@@ -55,11 +55,17 @@ const testUser = {
   lastName: 'test',
 };
 
+const testUser2 = { ...testUser, username: 'clustertest2' };
+
 describe('Cluster route testing', (): void => {
   let cookieHeader: string[];
   beforeAll(async () => {
     const response = await request(app).post('/api/auth').send(testUser);
     cookieHeader = response.headers['set-cookie'];
+  });
+
+  afterEach(async (): Promise<void> => {
+    await Cluster.deleteOne({ testCluster1 });
   });
 
   describe('POST /cluster', (): void => {
@@ -69,7 +75,13 @@ describe('Cluster route testing', (): void => {
         response = await request(app)
           .post('/api/cluster')
           .set('Cookie', cookieHeader)
-          .send(testClusterOne);
+          .send(testCluster1);
+      });
+
+      it('should add a cluster to the database', async (): Promise<void> => {
+        const newCluster = await Cluster.findOne({ testCluster1 });
+        expect(newCluster).toBeDefined();
+        expect(newCluster).not.toBeNull();
       });
 
       it('should respond with status 201', (): void => {
@@ -79,56 +91,93 @@ describe('Cluster route testing', (): void => {
       it('should respond with a success object', (): void => {
         expect(response.body).toHaveProperty('success', true);
       });
+    });
 
-      it('should add a cluster to the database', async (): Promise<void> => {
-        const newCluster = await Cluster.findOne({ testClusterOne });
-        expect(newCluster).toBeDefined();
-        expect(newCluster).not.toBeNull();
+    describe('given valid cluster data but no auth', (): void => {
+      let response: request.Response;
+      beforeAll(async (): Promise<void> => {
+        response = await request(app).post('/api/cluster').send(testCluster1);
+      });
+
+      it('should respond with status 400', (): void => {
+        expect(response.status).toBe(400);
+      });
+
+      it('should not add a new cluster to the database', async (): Promise<void> => {
+        const cluster = await Cluster.findOne({ testCluster1 });
+        expect(cluster).toBeNull();
+      });
+    });
+
+    describe('given invalid cluster data but correct auth', (): void => {
+      let response: request.Response;
+
+      beforeAll(async (): Promise<void> => {
+        response = await request(app)
+          .post('/api/cluster')
+          .set('Cookie', cookieHeader)
+          .send(badCluster);
+      });
+
+      it('should respond with status 500', (): void => {
+        expect(response.status).toBe(500);
+      });
+
+      it('should not add a new cluster to the database', async (): Promise<void> => {
+        const cluster = await Cluster.findOne({ testCluster1 });
+        expect(cluster).toBeNull();
       });
     });
 
     afterAll(async (): Promise<void> => {
-      await Cluster.deleteOne({ testClusterOne });
+      await User.deleteOne({ username: testUser.username });
     });
   });
 
-  describe('given valid cluster data but no auth', (): void => {
-    let response: request.Response;
-    beforeAll(async (): Promise<void> => {
-      response = await request(app).post('/api/cluster').send(testClusterOne);
-    });
+  // describe('GET /cluster', (): void => {
+  //   let cookieHeader1: string[];
+  //   let cookieHeader2: string[];
 
-    it('should respond with status 400', (): void => {
-      expect(response.status).toBe(400);
-    });
+  //   beforeAll(async (): Promise<void> => {
+  //     const response1 = await request(app).post('/api/auth').send(testUser);
+  //     cookieHeader1 = response1.headers['set-cookie'];
+  //     const response2 = await request(app).post('/api/auth').send(testUser2);
+  //     cookieHeader2 = response2.headers['set-cookie'];
 
-    it('should not add a new cluster to the database', async (): Promise<void> => {
-      const cluster = await Cluster.findOne({ testClusterOne });
-      expect(cluster).toBeNull();
-    });
-  });
+  //     await request(app)
+  //       .post('/api/cluster')
+  //       .set('Cookie', cookieHeader1)
+  //       .send(testCluster1);
 
-  describe('given invalid cluster data but correct auth', (): void => {
-    let response: request.Response;
+  //     await request(app)
+  //       .post('/api/cluster')
+  //       .set('Cookie', cookieHeader2)
+  //       .send(testCluster2);
+  //   });
 
-    beforeAll(async (): Promise<void> => {
-      response = await request(app)
-        .post('/api/cluster')
-        .set('Cookie', cookieHeader)
-        .send(badCluster);
-    });
+  //   describe('given valid auth', (): void => {
+  //     let response: request.Response;
+  //     beforeAll(async (): Promise<void> => {
+  //       response = await request(app)
+  //         .get('/api/cluster')
+  //         .set('Cookie', cookieHeader1);
+  //     });
 
-    it('should respond with status 500', (): void => {
-      expect(response.status).toBe(500);
-    });
+  //     it('should respond with status 200', (): void => {
+  //       expect(response.status).toBe(200);
+  //     });
 
-    it('should not add a new cluster to the database', async (): Promise<void> => {
-      const cluster = await Cluster.findOne({ testClusterOne });
-      expect(cluster).toBeNull();
-    });
-  });
+  //     it('should respond with an array of clusters', (): void => {
+  //       expect(response.body).toBeInstanceOf(Array);
+  //       expect(response.body.length).toBe(1);
+  //     });
+  //   });
 
-  afterAll(async (): Promise<void> => {
-    await User.deleteOne({ username: testUser.username });
-  });
+  //   afterAll(async (): Promise<void> => {
+  //     await User.deleteOne({ testUser });
+  //     await User.deleteOne({ testUser2 });
+  //     await Cluster.deleteOne({ testCluster1 });
+  //     await Cluster.deleteOne({ testCluster2 });
+  //   });
+  // });
 });
